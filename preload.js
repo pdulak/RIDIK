e2f2886a498b899a192e3e87819b77e4343ec4d3
Pawel Dulak <pawel.dulak@gmail.com>
2023-04-24 19:27:36 +0200

    feat: allow file access and load config on start

--- preload.js
+++ preload.js
@@ -1,4 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron')
+const path = require('path')
+const fs = require('fs')
+const configRootPath = path.resolve(__dirname,'config.json');
+const config = JSON.parse(fs.readFileSync(configRootPath, { encoding: 'utf-8' }));
 
 contextBridge.exposeInMainWorld('versions', {
     node: () => process.versions.node,
@@ -11,3 +15,7 @@ contextBridge.exposeInMainWorld('darkMode', {
     toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
     system: () => ipcRenderer.invoke('dark-mode:system')
 })
+
+contextBridge.exposeInMainWorld('config', {
+    config : config
+});
--- renderer.js
+++ renderer.js
@@ -1,5 +1,6 @@
 const information = document.getElementById('info')
 information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`
+information.innerHTML += `<hr> Config contents: ${JSON.stringify(config.config)}`
 
 document.getElementById('toggle-dark-mode').addEventListener('click', async () => {
     const isDarkMode = await window.darkMode.toggle()
@@ -10,3 +11,4 @@ document.getElementById('reset-to-system').addEventListener('click', async () =>
     await window.darkMode.system()
     document.getElementById('theme-source').innerHTML = 'System'
 })
+
