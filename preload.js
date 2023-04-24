978303293509933ae8ae4d5433a4c65eeed376ec
Pawel Dulak <pawel.dulak@gmail.com>
2023-04-24 16:46:09 +0200

    feat: dark theme switcher

---
+++ preload.js
@@ -0,0 +1,13 @@
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('versions', {
+    node: () => process.versions.node,
+    chrome: () => process.versions.chrome,
+    electron: () => process.versions.electron,
+    // we can also expose variables, not just functions
+})
+
+contextBridge.exposeInMainWorld('darkMode', {
+    toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
+    system: () => ipcRenderer.invoke('dark-mode:system')
+})
