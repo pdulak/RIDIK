4a42002f14aafc5f2c1cd640bec9694a94c1feb1
Pawel Dulak <pawel.dulak@gmail.com>
2023-04-25 10:16:09 +0200

    feat: first completion test

---
+++ js/openai.js
@@ -0,0 +1,40 @@
+const OPENAI_API_KEY = config.config.openaiApiKey;
+const OPENAI_ORGANIZATION = config.config.openaiOrganizationID;
+const OPENAI_URL = 'https://api.openai.com/'
+const OPENAI_MODEL = 'gpt-3.5-turbo'
+
+function getModels(destination) {
+    fetch(`${OPENAI_URL}v1/models`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Bearer ${OPENAI_API_KEY}`,
+            'OpenAI-Organization': OPENAI_ORGANIZATION
+        }
+    })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById(destination).innerHTML = JSON.stringify(data);
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function testCompletion(destination) {
+    fetch(`${OPENAI_URL}v1/chat/completions`, {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${OPENAI_API_KEY}`,
+            'OpenAI-Organization': OPENAI_ORGANIZATION,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            "model": OPENAI_MODEL,
+            "messages": [{"role": "user", "content": "Hello!"}]
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            document.getElementById(destination).innerHTML = JSON.stringify(data)
+        })
+        .catch(error => console.error(error));
+}
