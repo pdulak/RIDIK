68402575174df1958e231f12fc4036757672206a
Pawel Dulak <pawel.dulak@gmail.com>
2023-04-25 09:07:04 +0200

    feat: navigation introduced

--- renderer.js
+++ renderer.js
@@ -12,3 +12,25 @@ document.getElementById('reset-to-system').addEventListener('click', async () =>
     document.getElementById('theme-source').innerHTML = 'System'
 })
 
+const navLinks = document.querySelectorAll('nav a')
+
+navLinks.forEach((link) => {
+    link.addEventListener('click', handleClick);
+});
+
+function handleClick(event) {
+    event.preventDefault();
+    const sectionId = event.target.getAttribute('data-section');
+    const sectionElement = document.getElementById(sectionId);
+
+    if (sectionElement) {
+        // Hide all other sections
+        const allSections = document.querySelectorAll('[class=dynamic-section]');
+        allSections.forEach((section) => {
+            section.style.display = 'none';
+        });
+
+        // Make the clicked section visible
+        sectionElement.style.display = '';
+    }
+}
