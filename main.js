999a9308898dbebc67219acf6cbf9a32543b8095
Pawel Dulak <pawel.dulak@gmail.com>
2023-04-25 09:07:23 +0200

    feat: toggle dev window by F12

--- main.js
+++ main.js
@@ -14,6 +14,31 @@ function createWindow () {
     })
 
     win.loadFile('index.html')
+    addToggleDevToolsToWindow(win);
+}
+
+function addToggleDevToolsToWindow(win) {
+    win.webContents.on("before-input-event", (e, input) => {
+        if (input.type === "keyDown" && input.key === "F12") {
+            win.webContents.toggleDevTools();
+
+            win.webContents.on('devtools-opened', () => {
+                // Can't use mainWindow.webContents.devToolsWebContents.on("before-input-event") - it just doesn't intercept any events.
+                win.webContents.devToolsWebContents.executeJavaScript(`
+                            new Promise((resolve)=> {
+                              addEventListener("keydown", (event) => {
+                                if (event.key === "F12") {
+                                  resolve();
+                                }
+                              }, { once: true });
+                            })
+                          `)
+                    .then(() => {
+                        win.webContents.toggleDevTools();
+                    });
+            });
+        }
+    });
 }
 
 ipcMain.handle('dark-mode:toggle', () => {
