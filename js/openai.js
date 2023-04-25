6dfc85fceff450179b75acdaf3fc1d37e9d449a2
Pawel Dulak <pawel.dulak@gmail.com>
2023-04-25 11:31:24 +0200

    refactor: debug area and result formatting

--- js/openai.js
+++ js/openai.js
@@ -4,9 +4,9 @@ const OPENAI_URL = 'https://api.openai.com/'
 const OPENAI_MODEL = 'gpt-3.5-turbo'
 
 function getModels(destination) {
-    const destElement = document.getElementById(destination);
-    destElement.innerHTML = '';
-    destElement.ariaBusy = 'true';
+    const destinationElement = document.getElementById(destination);
+    destinationElement.innerHTML = '';
+    destinationElement.ariaBusy = 'true';
 
     fetch(`${OPENAI_URL}v1/models`, {
         method: 'GET',
@@ -17,17 +17,18 @@ function getModels(destination) {
     })
         .then(response => response.json())
         .then(data => {
-            document.getElementById(destination).innerHTML = JSON.stringify(data);
-            destElement.ariaBusy = 'false';
+            destinationElement.innerHTML = JSON.stringify(data);
+            destinationElement.ariaBusy = 'false';
         })
         .catch(error => console.error('Error:', error));
 }
 
 function testCompletion(destination) {
     const userContent = document.getElementById('openai-prompt').value;
-    const destElement = document.getElementById(destination);
-    destElement.innerHTML = '';
-    destElement.ariaBusy = 'true';
+    const destinationElement = document.getElementById(destination);
+    const destinationDebug = document.getElementById(destination + '-debug');
+    destinationElement.innerHTML = '';
+    destinationElement.ariaBusy = 'true';
 
     fetch(`${OPENAI_URL}v1/chat/completions`, {
         method: 'POST',
@@ -43,17 +44,21 @@ function testCompletion(destination) {
     })
         .then(response => response.json())
         .then(data => {
-            document.getElementById(destination).innerHTML = JSON.stringify(data);
-            destElement.ariaBusy = 'false';
+            destinationElement.innerHTML = data.choices[0].message.content.split("\n").join("<br />");
+            destinationElement.ariaBusy = 'false';
+            if (destinationDebug) {
+                destinationDebug.innerHTML = JSON.stringify(data);
+            }
         })
         .catch(error => console.error(error));
 }
 
 function testSpanish(destination) {
     const userContent = document.getElementById('openai-prompt').value;
-    const destElement = document.getElementById(destination);
-    destElement.innerHTML = '';
-    destElement.ariaBusy = 'true';
+    const destinationElement = document.getElementById(destination);
+    const destinationDebug = document.getElementById(destination + '-debug');
+    destinationElement.innerHTML = '';
+    destinationElement.ariaBusy = 'true';
 
     fetch(`${OPENAI_URL}v1/chat/completions`, {
         method: 'POST',
@@ -68,16 +73,18 @@ function testSpanish(destination) {
                 {'role': 'user', 'content': userContent},
                 {'role': 'system', 'content': '' +
                         'Jesteś nauczycielem hiszpańskiego. ' +
-                        'Użytkownik to twój uczeń. Jest na poziomie A1 - zaczyna naukę. ' +
-                        'Konstruuj przykłady w bardzo prosty sposób. ' +
-                        'Nie dodawaj na końcu pytania o to czy uczeń zrozumiał, ani powitania na początku.'}
+                        'Użytkownik to twój uczeń. Jest na poziomie A1. ' +
+                        'Nie dodawaj pytania o to czy zrozumiał, ani powitania.'}
             ]
         })
     })
         .then(response => response.json())
         .then(data => {
-            destElement.innerHTML = JSON.stringify(data);
-            destElement.ariaBusy = 'false';
+            destinationElement.innerHTML = data.choices[0].message.content.split("\n").join("<br />")
+            destinationElement.ariaBusy = 'false';
+            if (destinationDebug) {
+                destinationDebug.innerHTML = JSON.stringify(data);
+            }
         })
         .catch(error => console.error(error));
 }
