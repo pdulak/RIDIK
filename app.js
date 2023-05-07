86a2f81f770dadc312cc6c1cce8ff9d4ad029814
Pawel Dulak <pawel.dulak@gmail.com>
2023-05-07 18:37:11 +0200

    refactor: cleanup

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import {initialize, getModels, simpleCommandWithStreaming} from "./js/modules/openai.js";
+import {initialize, simpleCommandWithStreaming} from "./js/modules/openai.js";
 import { helloapi, moderation, inprompt, blogger } from "./js/tasksSolutions.js";
 
 initialize(config.openaiApiKey, config.openaiOrganizationID);
