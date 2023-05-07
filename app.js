88468b863b82309caa6ea752d8d4963260eadf68
Pawel Dulak <pawel.dulak@gmail.com>
2023-05-07 16:10:38 +0200

    feat: use commands to handle user prompt

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
-import { initialize, getModels, testCompletion, testSpanish } from "./js/modules/openai.js";
+import { initialize, getModels, testCompletion, testSpanish, simpleCommandExecution } from "./js/modules/openai.js";
 import { helloapi, moderation, inprompt, blogger } from "./js/tasksSolutions.js";
 
 initialize(config.openaiApiKey, config.openaiOrganizationID);
 
 let commands;
 
+const handleCommand = () => {
+    let commandContents = "You are a helpful assistant. Respond in a language in which user asked the question.";
+    const command = commands.find(command => command.name === document.getElementById("commands-selection").value);
+    if (command) {
+        commandContents = command.value;
+    }
+    simpleCommandExecution(commandContents);
+}
+
 document.getElementById("get-models").addEventListener("click", () => getModels("openai-results"));
-document.getElementById("test-completion").addEventListener("click", () => testCompletion("openai-results"));
-document.getElementById("test-spanish").addEventListener("click", () => testSpanish("openai-results"));
+// document.getElementById("test-completion").addEventListener("click", () => testCompletion("openai-results"));
+// document.getElementById("test-spanish").addEventListener("click", () => testSpanish("openai-results"));
 document.getElementById("hello-api-task").addEventListener("click", helloapi);
 document.getElementById("moderation-task").addEventListener("click", moderation);
 document.getElementById("inprompt-task").addEventListener("click", inprompt);
 document.getElementById("blogger-task").addEventListener("click", blogger);
+document.getElementById("run-command").addEventListener("click", handleCommand);
 
 // resize edit field if needed
 const openaiPrompt = document.getElementById('openai-prompt');
@@ -34,3 +44,4 @@ const offCommandsReceived = window.electron.on("commandsReceived", (commandsRece
                 ${command.description}
             </option>`).join("");
 });
+
