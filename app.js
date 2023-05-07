dde0234ccd6f0a6650db389a53867133acff3e1b
Pawel Dulak <pawel.dulak@gmail.com>
2023-05-07 15:55:41 +0200

    feat: display commands in select box

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import { helloapi, moderation, inprompt, blogger } from "./js/tasksSolutions.js"
 
 initialize(config.openaiApiKey, config.openaiOrganizationID);
 
+let commands;
+
 document.getElementById("get-models").addEventListener("click", () => getModels("openai-results"));
 document.getElementById("test-completion").addEventListener("click", () => testCompletion("openai-results"));
 document.getElementById("test-spanish").addEventListener("click", () => testSpanish("openai-results"));
@@ -18,10 +20,17 @@ openaiPrompt.addEventListener('input', () => {
     openaiPrompt.style.height = `${openaiPrompt.scrollHeight}px`;
 });
 
-document.getElementById('testButton').addEventListener('click', function () {
-    window.daoFunctions.getCommands();
-});
+window.daoFunctions.getCommands();
 
-const offCommandsReceived = window.electron.on("commandsReceived", (commands) => {
-    console.log('received', commands);
+const offCommandsReceived = window.electron.on("commandsReceived", (commandsReceived) => {
+    commands = commandsReceived.map(command => command.dataValues);
+
+    // get selectbox of ID = commandsSelection and fill options using commandsReceived
+    document.getElementById("commands-selection").innerHTML =
+        `<option value="">Helpful assistant</option>` +
+    commands.map(
+        command => `
+            <option value="${command.name}">
+                ${command.description}
+            </option>`).join("");
 });
