66423e19ace647470f3572f5d9073af6cbf6eeaf
Pawel Dulak <pawel.dulak@gmail.com>
2023-05-19 18:33:01 +0200

    feat: task processing finished

--- js/articleAuthor.js
+++ js/articleAuthor.js
@@ -1,7 +1,72 @@
 import { addMessage, addMessageInBackground, clearMessages, setModalBusy, setModalNotBusy } from "./modules/messagesModal.js";
 import { openai_completion } from "./modules/openai.js";
 
-const createArticlePart = async ({ task_id, title, parts, thisPart }) => {
+
+const buildArticle = async (taskStep, task) => {
+    const taskSteps = await window.daoFunctions.getTaskStepsForTask(task.dataValues.id);
+    const hasArticlePart = taskSteps.some(taskStep => taskStep.dataValues.type === 'create_article_part');
+    if (hasArticlePart) {
+        // check if all article parts are done
+        const articleParts = taskSteps.filter(taskStep => taskStep.dataValues.type === 'create_article_part');
+        const allArticlePartsDone = articleParts.every(taskStep => taskStep.dataValues.is_done);
+        if (allArticlePartsDone) {
+            addMessage("all article parts done");
+            const articleArray = articleParts.map(taskStep => {
+                const theResult = JSON.parse(taskStep.dataValues.results);
+                return theResult.choices[0].message.content;
+            });
+            const article = articleArray.join("\n\n");
+            taskStep.dataValues.results = article;
+            taskStep.dataValues.is_done = true;
+            taskStep = await window.daoFunctions.updateTaskStep(taskStep.dataValues);
+            task.dataValues.is_done = true;
+            task.dataValues.status = "done";
+            task = await window.daoFunctions.updateTask(task.dataValues);
+            addMessage(`article: ${article}`);
+        }
+    }
+}
+
+const createArticlePart = async (taskStep) => {
+    const theResult = await openai_completion(taskStep.dataValues.prompt);
+    addMessage(`create article part result: ${JSON.stringify(theResult)}`);
+    taskStep.dataValues.is_done = true;
+    taskStep.dataValues.results = JSON.stringify(theResult);
+    await window.daoFunctions.updateTaskStep(taskStep.dataValues);
+    return taskStep;
+};
+
+const createContents = async (taskStep, task) => {
+    const theResult = await openai_completion(taskStep.dataValues.prompt);
+    addMessage(`create contents result: ${JSON.stringify(theResult)}`);
+    const contents = theResult.choices[0].message.content.split("\n").filter(line => line.length > 0);
+    addMessage(`contents: ${JSON.stringify(contents)}`);
+    // create tasks for each part
+    contents.forEach(async (content, index) => {
+        // crete task step for each content
+        const articlePart = await createArticlePartTaskStep({
+            task_id: taskStep.dataValues.task_id,
+            title: task.dataValues.data,
+            contents: contents,
+            thisPart: content,
+        });
+        addMessage(`new task step created: ${JSON.stringify(articlePart.dataValues.id)}`);
+    });
+    // create article build step
+    const buildStep = await window.daoFunctions.createTaskStep({
+        task_id: taskStep.dataValues.task_id,
+        type: 'build_article',
+        status: 'created',
+    });
+    addMessage(`build step created: ${JSON.stringify(buildStep.dataValues.id)}`);
+    
+    taskStep.dataValues.is_done = true;
+    taskStep.dataValues.results = JSON.stringify(theResult);
+    await window.daoFunctions.updateTaskStep(taskStep.dataValues);
+    return taskStep;
+};
+
+const createArticlePartTaskStep = async ({ task_id, title, contents, thisPart }) => {
     const taskStep = await window.daoFunctions.createTaskStep({
         task_id: task_id,
         type: 'create_article_part',
@@ -13,47 +78,31 @@ Add nothing more, only two-three sentences related to "section"
 ${title}
 
 ### The contents:
-${parts.join("\n")}}
+${contents.join("\n")}}
 
 ### The section:
 ${thisPart}`,
         status: 'created',
     });
     addMessage(`new task step created: ${JSON.stringify(taskStep.dataValues)}`);
-    await processTaskStep(taskStep);
+    return await processTaskStep(taskStep);
 };
 
 const processTaskStep = async (taskStep, task) => {
     addMessage(`processing: ${JSON.stringify(taskStep.dataValues)}`);
     if (taskStep.dataValues.type === 'create_contents') {
-        const theResult = await openai_completion(taskStep.dataValues.prompt);
-        addMessage(`create contents result: ${JSON.stringify(theResult)}`);
-        const contents = theResult.choices[0].message.content.split("\n").filter(line => line.length > 0);
-        addMessage(`contents: ${JSON.stringify(contents)}`);
-        contents.forEach(async (content, index) => {
-            // crete task step for each content
-            const articlePart = await createArticlePart({
-                task_id: taskStep.dataValues.task_id,
-                title: task.dataValues.data,
-                parts: contents,
-                thisPart: content,
-            });
-            addMessage(`new task step created: ${JSON.stringify(articlePart.dataValues)}`);
-        });
-        taskStep.dataValues.is_done = true;
-        taskStep.dataValues.results = JSON.stringify(theResult);
-        await window.daoFunctions.updateTaskStep(taskStep.dataValues);
+        const result = await createContents(taskStep, task);
     }
     if (taskStep.dataValues.type === 'create_article_part') {
-        const theResult = await openai_completion(taskStep.dataValues.prompt);
-        addMessage(`create article part result: ${JSON.stringify(theResult)}`);
-        taskStep.dataValues.is_done = true;
-        taskStep.dataValues.results = JSON.stringify(theResult);
-        await window.daoFunctions.updateTaskStep(taskStep.dataValues);
+        const result = await createArticlePart(taskStep, task);
     }
+    if (taskStep.dataValues.type === 'build_article') {
+        const result = await buildArticle(taskStep, task);
+    }
+    return taskStep;
 }
 
-const createContents = async (task) => {
+const createContentsTaskStep = async (task) => {
     const taskStep = await window.daoFunctions.createTaskStep({
         task_id: task.dataValues.id,
         type: 'create_contents',
@@ -70,29 +119,33 @@ Add nothing more, only numbered list of points with contents of your article.
 }
 
 const processTask = async (task) => {
-    let maxSteps = 1;
+    let maxSteps = 8;
     while (maxSteps > 0) {
         if (task.dataValues.status === "created") {
-            addMessage(`processing task ${JSON.stringify(task.dataValues)}`);
+            addMessage(`processing task ${JSON.stringify(task.dataValues.data)}`);
             task.dataValues.status = 'processing';
             await window.daoFunctions.updateTask(task.dataValues);
         }
         if (task.dataValues.status === "processing") {
             addMessage(`looking for next step to take`);
             const taskSteps = await window.daoFunctions.getTaskStepsForTask(task.dataValues.id);
-            addMessage(`steps: ${JSON.stringify(taskSteps)}`);
+            addMessage(`steps: ${taskSteps.length}`);
             if (taskSteps.length === 0) {
-               taskSteps[0] = await createContents(task);
-            }
-            taskSteps.forEach(async (taskStep) => {
-                if (taskStep.dataValues.is_done === null) {
-                    await processTaskStep(taskStep, task);
+                taskSteps[0] = await createContentsTaskStep(task);
+            } else {
+                const firstUnfinishedStep = taskSteps.find(taskStep => taskStep.dataValues.is_done === null);
+                if (firstUnfinishedStep) {
+                    addMessage('before task processing');
+                    const result = await processTaskStep(firstUnfinishedStep, task);
+                    addMessage('after task processing');
                 }
-            });
+            }
         }
+        addMessage(`max steps left: ${maxSteps}`);
         maxSteps--;
     }
-    addMessage(`task ${JSON.stringify(task.dataValues)} finished due to max steps reached`);
+    addMessage(`task finished due to max steps reached`);
+    return true;
 }
 
 const newArticle = async () => {
